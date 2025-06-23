4e271a1321199dac7db994db9ee23970d6f3290c
Evgeniy Gorbenko <evgeniy.gorbenko2@gmail.com>
2025-06-23 13:00:14 +0300

    Добавил ручной ввод + задания 1-6 в одном коде

--- src/HW4.java
+++ src/HW4.java
@@ -9,9 +9,12 @@ public class HW4 {
         int lenght = scanner.nextInt();
         int[] array = new int[lenght];
         for (int i = 0; i < lenght; i++) {
-            array[i] = (int) (Math.random() * 100);
-            System.out.println(array[i]);
+            array[i] = scanner.nextInt();
         }
+     //   for (int i = 0; i < lenght; i++) {
+     //       array[i] = (int) (Math.random() * 100); //Случайные числа
+     //       System.out.println(array[i]);
+     //   }
         for (int i = array.length - 1; i >= 0; i--) {
             System.out.println(array[i]);
         }
@@ -42,13 +45,10 @@ public class HW4 {
         array[1] = array[array.length - 1 - 1];
         array[array.length - 1 - 1] = b;
         System.out.print("Заменены 1 и 4, 2 и 3 элементы массива местами:" + " ");
-        System.out.print(Arrays.toString(array));
-        scanner.close();
+        System.out.println(Arrays.toString(array));
 
-        //№6
-        /*int[] array = {1, 2, 5, 9, 12};
         boolean result = true;
-        for (int i = 1; i < array.length; i++) {
+        for (int i = 1; i < lenght; i++) {
             if (array[i] <= array[i - 1]) {
                 result = false;
                 break;
@@ -61,7 +61,10 @@ public class HW4 {
             System.out.println("Возрастающий");
         } else {
             System.out.println("Не возрастающий");
-        }*/
+        }
+        scanner.close();
+
+
 
     }
 }
